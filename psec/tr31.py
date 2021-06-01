9f447547ec9a17ff130273f0ed57ad72e26a89d3
Konstantin Novichikhin <konstantin.novichikhin@gmail.com>
2021-05-31 23:46:31 -0400

    ....

--- psec/tr31.py
+++ psec/tr31.py
@@ -5,46 +5,17 @@ from psec import des as _des
 from psec import mac as _mac
 from psec import tools as _tools
 
-__all__ = ["wrap", "unwrap", "KeyBlock", "Header"]
-
-
-def derive_tdes_cmac_subkey(key: bytes) -> Tuple[bytes, bytes]:
-    """Derive two subkeys from a DES key. Each subkey is 8 bytes."""
-
-    def shift_left_1(in_bytes: bytes) -> bytes:
-        """Shift byte array left by 1 bit"""
-        in_bytes = bytearray(in_bytes)
-        in_bytes[0] = in_bytes[0] & 0b01111111
-        int_in = int.from_bytes(in_bytes, "big") << 1
-        return int.to_bytes(int_in, len(in_bytes), "big")
-
-    r64 = b"\x00\x00\x00\x00\x00\x00\x00\x1B"
-
-    s = _des.encrypt_tdes_ecb(key, b"\x00" * 8)
-
-    if s[0] & 0b10000000:
-        k1 = _tools.xor(shift_left_1(s), r64)
-    else:
-        k1 = shift_left_1(s)
-
-    if k1[0] & 0b10000000:
-        k2 = _tools.xor(shift_left_1(k1), r64)
-    else:
-        k2 = shift_left_1(k1)
-
-    return k1, k2
+__all__ = ["TR31KeyBlock", "TR31Header"]
 
 
 class TR31Header:
     _mac_len = {"A": 4, "B": 8, "C": 4, "D": 16}
-
     _block_size = {"A": 8, "B": 8, "C": 8, "D": 16}
-
     _valid_key_sizes = {
-        "A": {8, 16, 24},
-        "B": {16, 24},
-        "C": {8, 16, 24},
-        "D": {16, 24, 32},
+        "A": frozenset([8, 16, 24]),
+        "B": frozenset([8, 16, 24]),
+        "C": frozenset([8, 16, 24]),
+        "D": frozenset([16, 24, 32]),
     }
 
     def __init__(
@@ -56,24 +27,14 @@ class TR31Header:
         version_num: str = "00",
         exportability: str = "0",
         reserved: str = "00",
-        block_len: str = "0000",
     ) -> None:
         self.version_id = version_id
-        self.block_len = block_len
         self.key_usage = key_usage
         self.algorithm = algorithm
         self.mode_of_use = mode_of_use
         self.version_num = version_num
         self.exportability = exportability
         self.reserved = reserved
-
-    @property
-    def mac_len(self) -> int:
-        return self._mac_len[self.version_id]
-
-    @property
-    def algorithm_block_size(self) -> int:
-        return self._block_size[self.version_id]
 
     @property
     def version_id(self) -> str:
@@ -84,16 +45,6 @@ class TR31Header:
         if version_id not in {"A", "B", "C", "D"}:
             raise ValueError(f"Version ID is not supported: '{version_id}'")
         self._version_id = version_id
-
-    @property
-    def block_len(self) -> str:
-        return self._block_len
-
-    @block_len.setter
-    def block_len(self, block_len: str) -> None:
-        if len(block_len) != 4 or not _tools.ascii_numeric(block_len):
-            raise ValueError(f"Block length is invalid: '{block_len}'")
-        self._block_len = block_len
 
     @property
     def key_usage(self) -> str:
@@ -155,35 +106,28 @@ class TR31Header:
             raise ValueError(f"Reserved field is invalid: '{reserved}'")
         self._reserved = reserved
 
-    def dump(self, key_length: int, extra_pad: int = 0) -> str:
+    def dump(self, key_len: int) -> str:
         """Format TR-31 header into string"""
 
-        if key_length not in self._valid_key_sizes[self.version_id]:
+        if key_len not in self._valid_key_sizes[self.version_id]:
             valid_sizes = ", ".join(map(str, self._valid_key_sizes[self.version_id]))
             raise ValueError(
-                f"Key must be {valid_sizes} bytes long for key block version {self.version_id}"
+                f"Key length '{str(key_len)}' must be {valid_sizes} bytes for key block version {self.version_id}"
             )
 
-        block_size = self.algorithm_block_size
-        if extra_pad % block_size != 0:
-            raise ValueError(
-                f"Extra pad must be multiple of {str(block_size)} "
-                f"for key block version {self.version_id}"
-            )
-
-        minimum_pad = block_size - ((2 + key_length) % block_size)
+        block_size = self._block_size[self.version_id]
+        pad_len = block_size - ((2 + key_len) % block_size)
 
         block_len = (
             16  # mandatory header
             + 4  # key length's length in ASCII
-            + (key_length * 2)
-            + (minimum_pad * 2)
-            + (extra_pad * 2)
-            + (self.mac_len * 2)
+            + (key_len * 2)
+            + (pad_len * 2)
+            + (self._mac_len[self.version_id] * 2)
         )
 
         if block_len > 9992:
-            ValueError(f"Key block length {str(block_len)} exceeds maximum of 9992")
+            ValueError(f"Key block length '{str(block_len)}' exceeds maximum of 9992.")
 
         return (
             self.version_id
@@ -205,14 +149,13 @@ class TR31Header:
 
         if len(key_block) % 8 != 0:
             raise ValueError(
-                f"Key block must be multiple of 8: '{str(len(key_block))}'"
+                f"Key block length '{str(len(key_block))}' must be multiple of 8."
             )
 
         if len(key_block) < 16:
-            raise ValueError("Header must be at a minimum 16 characters long")
+            raise ValueError(f"Header length '{str(len(key_block))}' must be at a minimum 16 characters long.")
 
         self.version_id = key_block[0]
-        self.block_len = key_block[1:5]
         self.key_usage = key_block[5:7]
         self.algorithm = key_block[7]
         self.mode_of_use = key_block[8]
@@ -240,11 +183,15 @@ class TR31KeyBlock:
         is multiple of 8 bytes.
     """
 
+    _mac_len = {"A": 4, "B": 8, "C": 4, "D": 16}
+    _block_size = {"A": 8, "B": 8, "C": 8, "D": 16}
+    _max_key_len = {"A": 24, "B": 24, "C": 24, "D": 32}
+
     def __init__(self, kbpk: bytes, header: Optional[TR31Header] = None) -> None:
         self.kbpk = kbpk
         self.header = header or TR31Header()
 
-    def wrap(self, key: bytes, extra_pad: int = 0) -> str:
+    def wrap(self, key: bytes, masked_key_len: Optional[int] = None) -> str:
         r"""Wrap key into a TR-31 key block.
 
         Parameters
@@ -253,14 +200,11 @@ class TR31KeyBlock:
             A key to be wrapped.
             Must be a valid DES key for versions A, B and C.
             Must be a valid AES key for version D.
-        extra_pad : int, optional
-            Add extra bytes of random data to
-            the key to mask its true length.
-            Must be multiple of 8 for versions A, B and C (DES).
-            Must be multiple of 16 for version D (AES).
-            Default 0.
-            For example, to make double DES key appear as
-            a triple DES set extra_pad to 8.
+        masked_key_len : int, optional
+            Desired key length in bytes to mask true key length.
+            Must be 8, 16 or 24 for versions A, B and C (DES).
+            Must be 16, 24 or 32 for version D (AES).
+            Defaults to max key size.
 
         Returns
         -------
@@ -276,16 +220,23 @@ class TR31KeyBlock:
         Examples
         --------
         >>> import psec
-        >>> h = psec.tr31.TR31Header("B", "P0", "T","E","00","E")
+        >>> h = psec.tr31.TR31Header("B", "P0", "T","E","00","N")
         >>> kb = psec.tr31.TR31KeyBlock(kbpk=b"\xFF" * 16, header=h)
         >>> kb.wrap(key=b"\xEE" * 16)  # doctest: +SKIP
-        'B0080P0TE00E0000E30027FBC014F6B22182E2EA95E245D261CA45260DF616F7C8F203EC19BD1197'
+        'B0096P0TE00N0000342811F905093F2B797EB9248C1121C011C2AE41BEC63E33C9C2FDB320540D82327221AE9C5C34FB'
+        >>> kb.header.version_id
+        'B'
+        >>> kb.header.key_usage
+        'P0'
+        >>> kb.header.algorithm
+        'T'
+        >>> kb.header.mode_of_use
+        'E'
+        >>> kb.header.version_num
+        '00'
+        >>> kb.header.exportability
+        'N'
         """
-
-        if extra_pad % self.header.algorithm_block_size != 0:
-            raise ValueError(
-                f"Extra pad must be multiple of {str(self.header.algorithm_block_size)}: '{str(extra_pad)}'"
-            )
 
         try:
             wrap = self._wrap_dispatch[self.header.version_id]
@@ -294,7 +245,17 @@ class TR31KeyBlock:
                 f"Key block version ID is not supported: '{self.header.version_id}'"
             )
 
-        return wrap(self, self.header.dump(len(key), extra_pad), key, extra_pad)
+        if masked_key_len is None:
+            masked_key_len = max(self._max_key_len[self.header.version_id], len(key))
+        else:
+            masked_key_len = max(masked_key_len, len(key))
+
+        return wrap(
+            self,
+            self.header.dump(masked_key_len),
+            key,
+            masked_key_len - len(key),
+        )
 
     def unwrap(self, key_block: str) -> bytes:
         r"""Unwrap key from a TR-31 key block.
@@ -320,8 +281,20 @@ class TR31KeyBlock:
         --------
         >>> import psec
         >>> kb = psec.tr31.TR31KeyBlock(kbpk=b"\xFF" * 16)
-        >>> kb.unwrap("B0080P0TE00E0000E30027FBC014F6B22182E2EA95E245D261CA45260DF616F7C8F203EC19BD1197")
+        >>> kb.unwrap("B0096P0TE00N0000342811F905093F2B797EB9248C1121C011C2AE41BEC63E33C9C2FDB320540D82327221AE9C5C34FB")
         b'\xee\xee\xee\xee\xee\xee\xee\xee\xee\xee\xee\xee\xee\xee\xee\xee'
+        >>> kb.header.version_id
+        'B'
+        >>> kb.header.key_usage
+        'P0'
+        >>> kb.header.algorithm
+        'T'
+        >>> kb.header.mode_of_use
+        'E'
+        >>> kb.header.version_num
+        '00'
+        >>> kb.header.exportability
+        'N'
         """
 
         try:
@@ -332,11 +305,11 @@ class TR31KeyBlock:
         if key_block_len != len(key_block):
             raise ValueError(
                 f"Key block length '{key_block[1:5]}' "
-                f"doesn't match data length '{str(key_block_len)}'"
+                f"doesn't match data length '{str(len(key_block))}'"
             )
 
         header_len = self.header.load(key_block)
-        mac_len = self.header.mac_len
+        mac_len = self._mac_len[self.header.version_id]
         key_block_mac = key_block[header_len:][-mac_len * 2 :]
         key_block_key = key_block[header_len:][: -mac_len * 2]
 
@@ -353,7 +326,7 @@ class TR31KeyBlock:
         except ValueError:
             raise ValueError(f"Encrypted key is invalid: '{key_block_key}'")
 
-        block_size = self.header.algorithm_block_size
+        block_size = self._block_size[self.header.version_id]
         if len(enc_key) < block_size or len(enc_key) % block_size != 0:
             raise ValueError(
                 f"Encrypted key length must be multiple of {str(block_size)}: '{key_block_key}'"
@@ -370,7 +343,7 @@ class TR31KeyBlock:
 
     # Version B
 
-    def _b_wrap(self, header: str, key: bytes, extra_pad: int = 0) -> str:
+    def _b_wrap(self, header: str, key: bytes, extra_pad: int) -> str:
         """Wrap key into TR-31 key block version B"""
 
         if len(self.kbpk) not in {16, 24}:
@@ -378,9 +351,9 @@ class TR31KeyBlock:
                 f"KBPK must be a double or triple DES key: '{str(len(self.kbpk))}'"
             )
 
-        if len(key) not in {16, 24}:
+        if len(key) not in {8, 16, 24}:
             raise ValueError(
-                f"Key must be a double or triple DES key: '{str(len(key))}'"
+                f"Key must be a single, double or triple DES key: '{str(len(key))}'"
             )
 
         if len(key) > len(self.kbpk):
@@ -456,7 +429,7 @@ class TR31KeyBlock:
         kbek = bytearray()  # encryption key
         kbak = bytearray()  # authentication key
 
-        k1, _ = derive_tdes_cmac_subkey(self.kbpk)
+        k1, _ = self._derive_des_cmac_subkey(self.kbpk)
 
         for i in range(1, len(self.kbpk) // 8 + 1):
             # Counter is incremented for each 8 byte block
@@ -474,15 +447,41 @@ class TR31KeyBlock:
 
     def _b_generate_mac(self, kbak: bytes, header: str, key_data: bytes) -> bytes:
         """Generate MAC using KBAK"""
-        km1, _ = derive_tdes_cmac_subkey(kbak)
+        km1, _ = self._derive_des_cmac_subkey(kbak)
         mac_data = header.encode("ascii") + key_data
         mac_data = mac_data[:-8] + _tools.xor(mac_data[-8:], km1)
         mac = _mac.generate_cbc_mac(kbak, mac_data, 1)
         return mac
 
+    def _derive_des_cmac_subkey(self, key: bytes) -> Tuple[bytes, bytes]:
+        """Derive two subkeys from a DES key. Each subkey is 8 bytes."""
+
+        def shift_left_1(in_bytes: bytes) -> bytes:
+            """Shift byte array left by 1 bit"""
+            in_bytes = bytearray(in_bytes)
+            in_bytes[0] = in_bytes[0] & 0b01111111
+            int_in = int.from_bytes(in_bytes, "big") << 1
+            return int.to_bytes(int_in, len(in_bytes), "big")
+
+        r64 = b"\x00\x00\x00\x00\x00\x00\x00\x1B"
+
+        s = _des.encrypt_tdes_ecb(key, b"\x00" * 8)
+
+        if s[0] & 0b10000000:
+            k1 = _tools.xor(shift_left_1(s), r64)
+        else:
+            k1 = shift_left_1(s)
+
+        if k1[0] & 0b10000000:
+            k2 = _tools.xor(shift_left_1(k1), r64)
+        else:
+            k2 = shift_left_1(k1)
+
+        return k1, k2
+
     # Version A, C.
 
-    def _c_wrap(self, header: str, key: bytes, extra_pad: int = 0) -> str:
+    def _c_wrap(self, header: str, key: bytes, extra_pad: int) -> str:
         """Wrap key into TR-31 key block version A or C"""
 
         if len(self.kbpk) not in {8, 16, 24}:
