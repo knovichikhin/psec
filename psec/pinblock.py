fc96665a6deb3b4eac9d93052a8b8eeb3bffb506
David Schmid <david.schmid@worldline.com>
2022-11-21 18:19:29 +0100

    Add PIN block ISO 4 support to project: - Add method encode_pin_field_iso_4 to pinblock.py - Add method encode_pan_field_iso_4 to pinblock.py - Add method encipher_pinblock_iso_4 to pinblock.py - Add method decode_pin_field_iso_4 to pinblock.py - Add method decipher_pinblock_iso_4 to pinblock.py - Add test cases to test_pinblock.py - Revise setup.cfg caused by updating flake8 from 5 to 6

--- psec/pinblock.py
+++ psec/pinblock.py
@@ -1,19 +1,26 @@
 r"""PIN blocks are data blocks that contain PIN, pad characters and sometimes
-other additional information, such as the leght of the PIN.
+other additional information, such as the length of the PIN.
 """
 
 import binascii as _binascii
 import secrets as _secrets
 
 from psec import tools as _tools
+from psec.aes import encrypt_aes_ecb, decrypt_aes_ecb
+from typing import Optional
 
 __all__ = [
     "encode_pinblock_iso_0",
     "encode_pinblock_iso_2",
     "encode_pinblock_iso_3",
+    "encode_pin_field_iso_4",
+    "encode_pan_field_iso_4",
+    "encipher_pinblock_iso_4",
     "decode_pinblock_iso_0",
     "decode_pinblock_iso_2",
     "decode_pinblock_iso_3",
+    "decode_pin_field_iso_4",
+    "decipher_pinblock_iso_4"
 ]
 
 
@@ -162,6 +169,157 @@ def encode_pinblock_iso_3(pin: str, pan: str) -> bytes:
     pan_block = b"\x00\x00" + _binascii.a2b_hex(pan[-13:-1])
 
     return _tools.xor(pinblock, pan_block)
+
+
+def encode_pin_field_iso_4(pin: str, det_pad: Optional[bytes] = None) -> bytes:
+    r"""Encode ISO 9564 PIN block format 4 plain text PIN field.
+    ISO format 4 PIN plain text PIN field is a 16 byte value that consits of
+
+        - Control field. A 4 bit hex value set to 4.
+        - PIN length. A 4 bit hex value in the range from 4 to C.
+        - PIN digits. Each digit is a 4 bit hex value in the range from 0 to 9.
+        - Fill digits. Each digit is a 4 bit hex value set to A.
+        - Random pad character. A 4 bit hex value in the range from 0 to F.
+
+    Parameters
+    ----------
+    pin :   str
+            ASCII Personal Identification Number.
+    det_pad (opt) : bytes
+                    Optional binary 8-byte deterministic padding to be used instead of random padding
+                    supporting deterministic test cases.
+
+    Returns
+    -------
+    Plain text PIN field : bytes
+        Binary 16-byte PIN field block.
+
+    Raises
+    ------
+    ValueError
+        PIN must be between 4 and 12 digits long.
+        Padding must be 8 bytes long.
+
+    Examples
+    --------
+    >>> from psec.pinblock import encode_pin_field_iso_4
+    >>> det_pad = bytes.fromhex("548ED7FD65495950")
+    >>> encode_pin_field_iso_4("1234", det_pad).hex().upper()
+    '441234AAAAAAAAAA548ED7FD65495950'
+    """
+
+    if len(pin) < 4 or len(pin) > 12 or not _tools.ascii_numeric(pin):
+        raise ValueError("PIN must be between 4 and 12 digits long")
+
+    if det_pad is None:
+        random_pad = "".join(_secrets.choice("0123456789ABCDEF") for _ in range(16))
+    else:
+        if len(det_pad) != 8:
+            raise ValueError("Padding must be 8 bytes long.")
+        random_pad = det_pad.hex().upper()
+
+    pin_len_hex = len(pin).to_bytes(1, "big").hex()[1]  # Only the low nibble is relevant, values 4 - C.
+    pinblock_str = "4" + pin_len_hex + pin + "A" * (14 - len(pin)) + random_pad
+    return _binascii.a2b_hex(pinblock_str)
+
+
+def encode_pan_field_iso_4(pan: str) -> bytes:
+    r"""Encode ISO 9564 PIN block format 4 plain text primary account number (PAN) field.
+    ISO format 4 plain text primary account number field is a 16 byte value that consits of
+
+        - PAN length. A 4 bit hex value in the range from 0 to 7 indicate a PAN length of 12 plus the value of the field
+                      (ranging from dec. 12 to 19). If the PAN is less than 12 digits, the digits are right justified and
+                      padded to the left with zeros and PAN length is set to 0.
+        - PAN digits. Each digit is a 4 bit hex value in the range from 0 to 9.
+        - Pad digits. A 4 bit hex value set to 0.
+
+    Parameters
+    ----------
+    pan :   str
+            ASCII Personal Account Number.
+
+    Returns
+    -------
+    Plain text PAN field :  bytes
+                            Binary 16-byte PAN field.
+
+    Raises
+    ------
+    ValueError
+        PAN must be between 1 and 19 digits long.
+
+    Examples
+    --------
+    >>> from psec.pinblock import encode_pan_field_iso_4
+    >>> encode_pan_field_iso_4("112233445566778899").hex().upper()
+    '61122334455667788990000000000000'
+    """
+
+    if len(pan) < 1 or len(pan) > 19 or not _tools.ascii_numeric(pan):
+        raise ValueError("PAN must be between 1 and 19 digits long.")
+
+    pan_len_field = "0" if len(pan) < 12 else str(len(pan) - 12)
+
+    if len(pan) < 12:
+        pan = pan.rjust(12, "0")
+
+    pan_field_str = (pan_len_field + pan).ljust(32, "0")
+
+    return _binascii.a2b_hex(pan_field_str)
+
+
+def encipher_pinblock_iso_4(key: bytes, pin: str, pan: str, det_pad: Optional[bytes] = None) -> bytes:
+    r"""Encrypt PIN with PAN binding according to ISO 9564 PIN block format 4. ISO format 4 is constructed using two
+    16-byte fields of PIN and PAN data respecively which are tied in the encryption process resulting in a 16-byte
+    enciphered PIN block.
+
+    The following steps are performed:
+
+        - Encode the PIN in the plain text PIN field.
+        - Encode the PAN in the plain text primary account number (PAN) field.
+        - Encipher the plain text PIN field with key K.
+        - Add the resulting intermedate block A modulo-2 (XOR) to the plain text PAN field.
+        - Encipher the resulting intermediate block B with the same key K.
+
+    Parameters
+    ----------
+    key :   bytes
+            Binary AES key.
+    pin :   str
+            ASCII Personal Identification Number.
+    pan :   str
+            ASCII Personal Account Number.
+    det_pad (opt) : bytes
+                    Optional binary 8-byte deterministic padding to be used instead of random padding
+                    supporting deterministic test cases.
+
+    Returns
+    -------
+    Enciphered PIN block :  bytes
+                            Binary 16-byte enciphered PIN block.
+
+    Raises
+    ------
+    ValueError
+        PIN must be between 4 and 12 digits long
+        Padding must be 8 bytes long.
+        PAN must be between 1 and 19 digits long.
+
+    Examples
+    --------
+    >>> from psec.pinblock import encipher_pinblock_iso_4
+    >>> key = bytes.fromhex("00112233445566778899AABBCCDDEEFF")
+    >>> pin = "1234"
+    >>> pan = "1234567890123456"
+    >>> det_pad = bytes.fromhex("1122334455667788")
+    >>> encipher_pinblock_iso_4(key, pin, pan, det_pad).hex().upper()
+    '7CDF645C86CAF763AE34637A66997534'
+    """
+    pin_field = encode_pin_field_iso_4(pin, det_pad)
+    pan_field = encode_pan_field_iso_4(pan)
+    intermediate_block_a = encrypt_aes_ecb(key, pin_field)
+    intermediate_block_b = _tools.xor(intermediate_block_a, pan_field)
+    return encrypt_aes_ecb(key, intermediate_block_b)
 
 
 def decode_pinblock_iso_0(pinblock: bytes, pan: str) -> str:
@@ -364,3 +522,116 @@ def decode_pinblock_iso_3(pinblock: bytes, pan: str) -> str:
         raise ValueError(f"PIN is not numeric: `{pin}`")
 
     return pin
+
+
+def decode_pin_field_iso_4(pin_field: bytes) -> str:
+    r"""Decode ISO 9564 PIN block format 4 plain text PIN field.
+        ISO format 4 PIN plain text PIN field is a 16 byte value that consits of
+
+            - Control field. A 4 bit hex value set to 4.
+            - PIN length. A 4 bit hex value in the range from 4 to C.
+            - PIN digits. Each digit is a 4 bit hex value in the range from 0 to 9.
+            - Fill digits. Each digit is a 4 bit hex value set to A.
+            - Random pad character. A 4 bit hex value in the range from 0 to F.
+
+    Parameters
+    ----------
+    pin_field : bytes
+                Binary 16-byte PIN field.
+
+    Returns
+    -------
+    pin : str
+          ASCII Personal Identification Number.
+
+    Raises
+    ------
+    ValueError
+        PIN field must be 16 bytes long
+        PIN field must be 32 hexchars long
+        PIN field is not ISO format 4: control field `X`
+        PIN field filler is incorrect: `filler`
+        PIN length must be between 4 and 12: `pin length`
+        PIN is not numeric: `pin`
+
+    Examples
+    --------
+    >>> from psec.pinblock import decode_pin_field_iso_4
+    >>> pin_field = bytes.fromhex("441234AAAAAAAAAA548ED7FD65495950")
+    >>> decode_pin_field_iso_4(pin_field)
+    '1234'
+    """
+
+    if len(pin_field) != 16:
+        raise ValueError("PIN field must be 16 bytes long")
+
+    pin_field_str = pin_field.hex().upper()
+
+    if pin_field_str[0] != "4":
+        raise ValueError(f"PIN block is not ISO format 4: control field `{pin_field_str[0]}`")
+
+    pin_len = int(pin_field_str[1], 16)
+
+    if pin_len < 4 or pin_len > 12:
+        raise ValueError(f"PIN length must be between 4 and 12: `{pin_len}`")
+
+    if pin_field_str[pin_len + 2: 16] != ("A" * (14-pin_len)):
+        raise ValueError(f"PIN block filler is incorrect: `{pin_field_str[pin_len+2: 16]}`")
+
+    pin = pin_field_str[2: pin_len + 2]
+
+    if not _tools.ascii_numeric(pin):
+        raise ValueError(f"PIN is not numeric: `{pin}`")
+
+    return pin
+
+
+def decipher_pinblock_iso_4(key: bytes, pin_block: bytes, pan: str) -> str:
+    r"""Decrypt ISO 9564 PIN block format 4 and extract PIN.
+
+    The following steps are performed:
+
+        - Decipher the PIN block with key K resulting in intermediate block B.
+        - Encode the PAN in the plain text primary account number (PAN) field.
+        - Add the intermediate block B modulo-2 (XOR) to the plain text PAN field, resulting in intermediate block A.
+        - Decipher the intermediate block A with the key K yielding the plain text PIN field.
+        - Decode the plain text PIN field and extract the PIN.
+
+    Parameters
+    ----------
+    key :   bytes
+            Binary AES key.
+    pin_block : bytes
+                Binary 16-byte enciphered PIN block.
+
+    Returns
+    -------
+    pin :   str
+            ASCII Personal Identification Number.
+
+    Raises
+    ------
+    ValueError
+        Data length must be multiple of AES block size 16.
+        PAN must be between 1 and 19 digits long.
+        PIN block is not ISO format 4: control field `X`
+        PIN block filler is incorrect: `filler`
+        PIN length must be between 4 and 12: `pin length`"
+        PIN is not numeric: `pin`
+
+
+    Examples
+    --------
+    >>> from psec.pinblock import decipher_pinblock_iso_4
+    >>> key = bytes.fromhex("00112233445566778899AABBCCDDEEFF")
+    >>> pan = "1234567890123456"
+    >>> pin_block = bytes.fromhex("E4BE5B623AF7E006AC319E5B93544564")
+    >>> decipher_pinblock_iso_4(key, pin_block, pan)
+    '1234'
+    """
+
+    intermediate_block_b = decrypt_aes_ecb(key, pin_block)
+    pan_field = encode_pan_field_iso_4(pan)
+    intermediate_block_a = _tools.xor(intermediate_block_b, pan_field)
+    pin_field = decrypt_aes_ecb(key, intermediate_block_a)
+    return decode_pin_field_iso_4(pin_field)
--- tests/test_pinblock.py
+++ tests/test_pinblock.py
@@ -1,6 +1,9 @@
 import pytest
 from psec import pinblock
 
+import os
+import secrets as _secrets
+
 
 # fmt: off
 @pytest.mark.parametrize(
@@ -95,6 +98,125 @@ def test_encode_pinblock_iso_3(pin: str, pan: str, pin_block: str) -> None:
     assert (
         pin_block
         == pinblock.encode_pinblock_iso_3(pin, pan).hex().upper()[: 2 + len(pin)]
+    )
+
+
+@pytest.mark.parametrize(
+    ["pin", "error"],
+    [
+        ("123", "PIN must be between 4 and 12 digits long"),
+        ("1234567890123", "PIN must be between 4 and 12 digits long"),
+    ],
+)
+def test_encode_pin_field_iso_4_exception(pin: str, error: str) -> None:
+    with pytest.raises(
+        ValueError,
+        match=error,
+    ):
+        pinblock.encode_pin_field_iso_4(pin)
+
+
+# deterministic
+@pytest.mark.parametrize(
+    ["pin", "det_pad", "pin_field"],
+    [
+        ("1234", "548ED7FD65495950", "441234AAAAAAAAAA548ED7FD65495950"),
+    ],
+)
+def test_encode_pin_field_iso_4(pin: str, det_pad: str, pin_field: str) -> None:
+    det_pad_bytes = bytes.fromhex(det_pad)
+    assert (
+        pin_field
+        == pinblock.encode_pin_field_iso_4(pin, det_pad_bytes).hex().upper()
+    )
+
+
+@pytest.mark.parametrize(
+    ["pan", "error"],
+    [
+        ("", "PAN must be between 1 and 19 digits long."),
+        ("12345678901234567890", "PAN must be between 1 and 19 digits long."),
+    ],
+)
+# fmt: on
+def test_encode_pan_field_iso_4_exception(pan: str, error: str) -> None:
+    with pytest.raises(
+        ValueError,
+        match=error,
+    ):
+        pinblock.encode_pan_field_iso_4(pan)
+
+
+@pytest.mark.parametrize(
+    ["pan", "pan_field"],
+    [
+        ("112233445566778899", "61122334455667788990000000000000"),
+    ],
+)
+# fmt: on
+def test_encode_pan_field_iso_4(pan: str, pan_field: str) -> None:
+    assert (
+        pan_field
+        == pinblock.encode_pan_field_iso_4(pan).hex().upper()
+    )
+
+
+# fmt: off
+@pytest.mark.parametrize(
+    ["key", "pin", "pan", "det_pad", "error"],
+    [
+        (b"1" * 16, "123", "55555555555555", None, "PIN must be between 4 and 12 digits long"),
+        (b"1" * 16, "1234567890123", "55555555555555", None, "PIN must be between 4 and 12 digits long"),
+        (b"1" * 16, "123A", "55555555555555", None, "PIN must be between 4 and 12 digits long"),
+        (b"1" * 16, "1234", "55555555555555", b"1" * 9, "Padding must be 8 bytes long."),
+        (b"1" * 16, "1234", "", None, "PAN must be between 1 and 19 digits long."),
+        (b"1" * 16, "1234", "555555555555555555555", None, "PAN must be between 1 and 19 digits long."),
+        (b"1" * 16, "1234", "5555555555555A", None, "PAN must be between 1 and 19 digits long."),
+    ],
+)
+# fmt: on
+def test_encipher_pinblock_iso_4_exception(key: bytes, pin: str, pan: str, det_pad: bytes, error: str) -> None:
+    with pytest.raises(
+        ValueError,
+        match=error,
+    ):
+        pinblock.encipher_pinblock_iso_4(key, pin, pan, det_pad)
+
+
+# deterministic
+@pytest.mark.parametrize(
+    ["key", "pin", "pan", "det_pad", "pin_block"],
+    [
+        # Test vector from ep2 - eft/pos 2000 Security Specification, Version 8.0.0, 8.4: PIN Encryption
+        (bytes.fromhex("C1D0F8FB4958670DBA40AB1F3752EF0D"),
+         "1234",
+         "432198765432109870",
+         bytes.fromhex("146C6601F4A8035C"),
+         "CC17F65586BFD0953010226C4FC5B3CA"),
+    ],
+)
+def test_encipher_pinblock_iso_4_det(key: bytes, pin: str, pan: str, det_pad: bytes, pin_block: str) -> None:
+    assert (
+        pin_block
+        == pinblock.encipher_pinblock_iso_4(key, pin, pan, det_pad).hex().upper()
+    )
+
+
+# non deterministic
+def test_encipher_decipher_pinblock_iso_4_non_det() -> None:
+    pin_len = _secrets.choice(range(4, 12))
+    pin_range_start = 10**(pin_len-1)
+    pin_range_end = (10**pin_len) - 1
+    pin = str(_secrets.choice(range(pin_range_start, pin_range_end)))
+    pan = "12334567890123456"
+    key_lens = [16, 24, 32]
+    key_len = _secrets.choice(key_lens)
+    key = os.urandom(key_len)
+    pin_block = pinblock.encipher_pinblock_iso_4(key, pin, pan)
+    pin_calc = pinblock.decipher_pinblock_iso_4(key, pin_block, pan)
+
+    assert (
+        pin == pin_calc
     )
 
 
@@ -206,3 +328,90 @@ def test_decode_pinblock_iso_3_exception(
 )
 def test_decode_pinblock_iso_3(pin: str, pin_block: bytes, pan: str) -> None:
     assert pin == pinblock.decode_pinblock_iso_3(pin_block, pan)
+
+
+# fmt: off
+@pytest.mark.parametrize(
+    ["pin_block", "error"],
+    [
+        (bytes.fromhex("441234AAAAAAAAAA548ED7FD6549595000"), "PIN field must be 16 bytes long"),
+        (bytes.fromhex("341261AAAAEDCB"), "PIN field must be 16 bytes long"),
+        (bytes.fromhex("541234AAAAAAAAAA548ED7FD65495950"), "PIN block is not ISO format 4: control field `5`"),
+        (bytes.fromhex("43123AAAAAAAAAAA548ED7FD65495950"), "PIN length must be between 4 and 12: `3`"),
+        (bytes.fromhex("441234ABCDEFABCD548ED7FD65495950"), "PIN block filler is incorrect: `ABCDEFABCD`"),
+        (bytes.fromhex("4412D4AAAAAAAAAA548ED7FD65495950"), "PIN is not numeric: `12D4`"),
+    ],
+)
+# fmt: on
+def test_decode_pinblock_iso_4_exception(pin_block: bytes, error: str) -> None:
+    with pytest.raises(
+        ValueError,
+        match=error,
+    ):
+        pinblock.decode_pin_field_iso_4(pin_block)
+
+
+@pytest.mark.parametrize(
+    ["pin_field", "pin"],
+    [
+        (bytes.fromhex("441234AAAAAAAAAA548ED7FD65495950"), "1234"),
+    ],
+)
+def test_decode_pinblock_iso_4(pin_field: bytes, pin) -> None:
+    assert pin == pinblock.decode_pin_field_iso_4(pin_field)
+
+
+@pytest.mark.parametrize(
+    ["key", "pin_block", "pan", "error"],
+    [
+        (bytes.fromhex("C1D0F8FB4958670DBA40AB1F3752EF0D"),
+         bytes.fromhex("CC17F65586BFD0953010226C4FC5B3CA00"),
+         "432198765432109870",
+         "Data length (17) must be multiple of AES block size 16."),
+
+        (bytes.fromhex("C1D0F8FB4958670DBA40AB1F3752EF0D"),
+         bytes.fromhex("CC17F65586BFD0953010226C4FC5B3CA"),
+         "43219876543210987099",
+         "PAN must be between 1 and 19 digits long."),
+
+        (bytes.fromhex("E60B15B90ABDF14CEE337C97440F0D6E"),
+         bytes.fromhex("7D5AF4C33667A2098626027FB7A9A1B7"),
+         "4266229809609384667",
+         "PIN block is not ISO format 4: control field `3`"),
+
+        (bytes.fromhex("C7BBEBA16C5AD97D5866450718C03750"),
+         bytes.fromhex("2F9C910447F17293EC95DEDE1E3CA201"),
+         "8220499325458689523",
+         "PIN is not numeric: `1AB2`"),
+
+        (bytes.fromhex("820F6C7C12355BDFF1AB6CE12E8EED89"),
+         bytes.fromhex("CCF310A8300B46C925A86B1098089301"),
+         "939589847393485609",
+         "PIN block filler is incorrect: `BBBBBBBBBB`"),
+
+        (bytes.fromhex("4FE3F0311936FBCE44F17159F1659CF09B2BF8913BB514A1"),
+         bytes.fromhex("51334B00A6CBA3EC7B1C6F871F060AFC"),
+         "2129100799029059903",
+         "PIN length must be between 4 and 12: `3`"),
+    ],
+)
+# fmt: on
+def test_decipher_pinblock_iso_4_exception(
+    key: bytes, pin_block: bytes, pan: str, error: str
+) -> None:
+    with pytest.raises(ValueError) as e:
+        pinblock.decipher_pinblock_iso_4(key, pin_block, pan)
+    assert e.value.args[0] == error
+
+
+@pytest.mark.parametrize(
+    ["key", "pin_block", "pan", "pin"],
+    [
+        # Test vector from ep2 - eft/pos 2000 Security Specification, Version 8.0.0, 8.4: PIN Encryption
+        (bytes.fromhex("C1D0F8FB4958670DBA40AB1F3752EF0D"),
+         bytes.fromhex("CC17F65586BFD0953010226C4FC5B3CA"),
+        "432198765432109870", "1234"),
+    ],
+)
+def test_decipher_pinblock_iso_4(key: bytes, pin_block: bytes, pan: str, pin: str) -> None:
+    assert pin == pinblock.decipher_pinblock_iso_4(key, pin_block, pan)
